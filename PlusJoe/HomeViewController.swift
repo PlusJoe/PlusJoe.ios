9b5ae5d68885c8b94af83f025a26cbd6a298a4d0
dmitryame <dmitry@custommode.com>
2015-03-24 21:05:17 -0400

    more settings

---
+++ PlusJoe/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewControlle.swift
+//  PlusJoe
+//
+//  Created by D on 3/24/15.
+//  Copyright (c) 2015 PlusJoe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+//    @IBAction func unwindToHome (segue : UIStoryboardSegue) {
+//        NSLog("SearchPosts seque from segue id: \(segue.identifier)")
+//    }
+    
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        NSLog("----------------seguiing")
+//        
+//        // Make sure your segue name in storyboard is the same as this line
+//        if (segue.identifier == "myNewPostSegue")
+//        {
+//            NSLog("----calling prepareForSegue myNewPostSegue")
+//        }
+//    }
+//    
+    
+//    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+//        if APP_DELEGATE.getCurrentLocation() != nil {
+//            return true
+//        } else {
+//            let alertMessage = UIAlertController(title: "Error", message: "Enable GPS, or wait few seconds for location to be detected and try again.", preferredStyle: UIAlertControllerStyle.Alert)
+//            let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in})
+//            alertMessage.addAction(ok)
+//            presentViewController(alertMessage, animated: true, completion: nil)
+//        }
+//        return false
+//    }
+    
+}
