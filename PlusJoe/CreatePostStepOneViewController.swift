79b9bcdba4e9e0b24e300f8f1a02987d843efe8b
dmitryame <dmitry@custommode.com>
2015-03-26 23:07:42 -0400

    implementing first step of creating a new post

---
+++ PlusJoe/CreatePostStepOneViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CreatePostStepOneViewController.swift
+//  PlusJoe
+//
+//  Created by D on 3/26/15.
+//  Copyright (c) 2015 PlusJoe. All rights reserved.
+//
+
+
+import UIKit
+
+class CreatePostStepOneViewController: UIViewController {
+    
+    @IBOutlet weak var backNavButton: UIBarButtonItem!
+    
+    @IBOutlet weak var sellButton: UIButton!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var thingButton: UIButton!
+    @IBOutlet weak var serviceButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBAction func backButtonAction(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        backNavButton.title = "\u{f053}"
+        if let font = UIFont(name: "FontAwesome", size: 20) {
+            backNavButton.setTitleTextAttributes([NSFontAttributeName: font], forState: UIControlState.Normal)
+        }
+        
+        
+        thingButton.hidden = true
+        serviceButton.hidden = true
+        backButton.hidden = true
+        nextButton.hidden = true
+    }
+    
+    @IBAction func sellButtonAction(sender: AnyObject) {
+        sellButton.backgroundColor = UIColor.greenColor()
+        buyButton.backgroundColor = UIColor.whiteColor()
+        
+        sellButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        buyButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        
+        thingButton.hidden = false
+        serviceButton.hidden = false
+    }
+    
+    @IBAction func buyButtonAction(sender: AnyObject) {
+        buyButton.backgroundColor = UIColor.greenColor()
+        sellButton.backgroundColor = UIColor.whiteColor()
+
+        buyButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        sellButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        
+        thingButton.hidden = false
+        serviceButton.hidden = false
+    }
+    
+    @IBAction func thingButtonAction(sender: AnyObject) {
+        thingButton.backgroundColor = UIColor.greenColor()
+        serviceButton.backgroundColor = UIColor.whiteColor()
+        
+        thingButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        serviceButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        
+        backButton.hidden = false
+        nextButton.hidden = false
+    }
+    
+    @IBAction func serviceButtonAction(sender: AnyObject) {
+        serviceButton.backgroundColor = UIColor.greenColor()
+        thingButton.backgroundColor = UIColor.whiteColor()
+        
+        serviceButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        thingButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        
+        backButton.hidden = false
+        nextButton.hidden = false
+
+    }
+    
+    
+}
+
---
+++ PlusJoe/CreatePostStepTwoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreatePostStepTwoViewController.swift
+//  PlusJoe
+//
+//  Created by D on 3/26/15.
+//  Copyright (c) 2015 PlusJoe. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostStepTwoViewController: UIViewController {
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var postBody: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
