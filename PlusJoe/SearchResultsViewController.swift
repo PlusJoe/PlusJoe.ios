495b568c73ca6bfc83ef323d706692b323e484eb
dmitryame <dmitry@custommode.com>
2015-04-11 10:57:01 -0400

    wip: responding to marker tap

--- PlusJoe/SearchResultsViewController.swift
+++ PlusJoe/SearchResultsViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import MapKit
 
-class SearchResultsViewController: UIViewController {
+class SearchResultsViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var backNavButton: UIBarButtonItem!
     
@@ -24,6 +24,7 @@ class SearchResultsViewController: UIViewController {
     
     var posts:[PJPost] = [PJPost]()
     
+    var currentPost = 0
     
     @IBAction func backButtonAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -41,13 +42,17 @@ class SearchResultsViewController: UIViewController {
         } else {
             searchingForLabel.text = "Showing all in your area"
         }
+        
+        mapView.delegate = self
     }
     
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         PJPost.search(CURRENT_LOCATION, searchText: searchString,
             succeeded: { (results) -> () in
+                self.posts = results
+//                self.resultNumber.text = "\(self.currentPost+1) of \(self.posts.count)"
                 if results.count == 0 {
                     let alertMessage = UIAlertController(title: nil, message: "Nothing found, try again.", preferredStyle: UIAlertControllerStyle.Alert)
                     let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
@@ -56,18 +61,18 @@ class SearchResultsViewController: UIViewController {
                     alertMessage.addAction(ok)
                     self.presentViewController(alertMessage, animated: true, completion: nil)
                 }
-                    self.posts = results
-                    var annotations = [MKPointAnnotation]()
+                
+                var annotations = [MKPointAnnotation]()
                 for var index = 0; index < self.posts.count; ++index {
-                        let annotation = MKPointAnnotation()
-                        let location = self.posts[index].location
-                        annotation.coordinate =
-                            CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude))
-                        annotation.title = "\(index + 1)"
-                        //                    annotation.subtitle = "\(index)"
-                        self.mapView.addAnnotation(annotation)
-                        annotations.append(annotation)
-                    }
+                    let annotation = MKPointAnnotation()
+                    let location = self.posts[index].location
+                    annotation.coordinate =
+                        CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude))
+                    annotation.title = "\(index + 1)"
+                    //                    annotation.subtitle = "\(index)"
+                    self.mapView.addAnnotation(annotation)
+                    annotations.append(annotation)
+                }
                 if annotations.count > 0 {
                     self.mapView.selectAnnotation(annotations[0], animated: true)
                     self.mapView.showAnnotations(annotations, animated: true)
@@ -80,8 +85,16 @@ class SearchResultsViewController: UIViewController {
                 })
                 alertMessage.addAction(ok)
                 self.presentViewController(alertMessage, animated: true, completion: nil)
-
+                
         }
     }
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        NSLog("selected annotation: \(view.annotation.title)")
+        currentPost = view.annotation.title!.toInt()!
+        resultNumber.text = "\(currentPost) of \(posts.count)"
+
+    }
+    
     
 }
