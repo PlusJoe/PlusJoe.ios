a9911d80d2fa0a12ffd21d656a86154fdbb2c6c4
dmitryame <dmitry@custommode.com>
2015-05-14 23:00:53 -0400

    added bookmarks model

---
+++ PlusJoe/PJBookmark.swift
@@ -0,0 +1,37 @@
+//
+//  PJBookmark.swift
+//  PlusJoe
+//
+//  Created by D on 5/14/15.
+//  Copyright (c) 2015 PlusJoe. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+let PJBOOKMARK:PJBookmark = PJBookmark()
+class PJBookmark: BaseDataModel {
+    let CLASS_NAME = "Bookmarks"
+    
+    let tag = "tag" //: String
+    let owner = "owner" //: String
+    
+    
+    class func loadUMyBookmarks(
+        succeeded:(bookmarks:[PFObject]) -> (),
+        failed:(error: NSError!) -> ()
+        ) -> () {
+            let bookmarksQuery = PFQuery(className:PJBOOKMARK.CLASS_NAME)
+            bookmarksQuery.whereKey(PJBOOKMARK.owner, equalTo: DEVICE_UUID)
+            
+            bookmarksQuery.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]?, error: NSError?) -> Void in
+                if error == nil {
+                    succeeded(bookmarks: objects as! [PFObject])
+                } else {
+                    // Log details of the failure
+                    failed(error: error)
+                }
+            })
+    }
+    
+}
